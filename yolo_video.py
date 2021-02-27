111dc0b1eae84d83b420351c2a4de4bea660443f
Eren Tekin <erntkn4@gmail.com>
2021-02-27 12:38:29 +0300

    added logging and window properties to parser

--- yolo_video.py
+++ yolo_video.py
@@ -13,14 +13,26 @@
 └── yolo_video.py
 if program cant find yolo folder in main folder it will crash."""
 # example usage: python yolo_video.py -i video.mp4 -o video_out.avi
-# if you want to resize video, uncomment line 110
 import argparse
 import glob
 import time
+import logging
+from pathlib import Path
 
 import cv2
-import imutils
 import numpy as np
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '%(asctime)s-%(name)s-%(message)s')
+
+file_handler = logging.FileHandler("deneme.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", type=str, default="",
@@ -29,21 +41,31 @@ parser.add_argument("-o", "--output", type=str, default="",
                     help="path to (optional) output video file")
 parser.add_argument("-d", "--display", type=int, default=1,
                     help="display output or not (1/0)")
+parser.add_argument("-ht", "--height", type=int, default=1200,
+                    help="height of output")
+parser.add_argument("-wt", "--width", type=int, default=700,
+                    help="width of output")
 parser.add_argument("-c", "--confidence", type=float, default=0.5,
                     help="confidence threshold")
 parser.add_argument("-t", "--threshold", type=float, default=0.4,
                     help="non-maximum supression threshold")
 
 args = vars(parser.parse_args())
+logger.info("Parsed Arguments")
 
 CONFIDENCE_THRESHOLD = args["confidence"]
 NMS_THRESHOLD = args["threshold"]
+if not Path(args["input"]).exists():
+    raise FileNotFoundError(
+        "Path to video file is not exist.")
+
 vc = cv2.VideoCapture(args["input"])
 weights = glob.glob("yolo/*.weights")[0]
 labels = glob.glob("yolo/*.txt")[0]
 cfg = glob.glob("yolo/*.cfg")[0]
 
-print("You are now using {} weights ,{} configs and {} labels.".format(weights, cfg, labels))
+logger.info("Using {} weights ,{} configs and {} labels.".format(
+    weights, cfg, labels))
 
 class_names = list()
 with open(labels, "r") as f:
@@ -59,10 +81,11 @@ layer = net.getLayerNames()
 layer = [layer[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 writer = None
 
+
 def detect(frm, net, ln):
     (H, W) = frm.shape[:2]
     blob = cv2.dnn.blobFromImage(frm, 1/255.0, (416, 416),
-        swapRB=True, crop=False)
+                                 swapRB=True, crop=False)
     net.setInput(blob)
     start_time = time.time()
     layerOutputs = net.forward(ln)
@@ -71,13 +94,12 @@ def detect(frm, net, ln):
     boxes = []
     classIds = []
     confidences = []
-
     for output in layerOutputs:
         for detection in output:
             scores = detection[5:]
             classID = np.argmax(scores)
             confidence = scores[classID]
-            
+
             if confidence > CONFIDENCE_THRESHOLD:
                 box = detection[0:4] * np.array([W, H, W, H])
                 (centerX, centerY, width, height) = box.astype("int")
@@ -88,7 +110,8 @@ def detect(frm, net, ln):
                 classIds.append(classID)
                 confidences.append(float(confidence))
 
-    idxs = cv2.dnn.NMSBoxes(boxes, confidences, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+    idxs = cv2.dnn.NMSBoxes(
+        boxes, confidences, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
 
     if len(idxs) > 0:
         for i in idxs.flatten():
@@ -97,26 +120,30 @@ def detect(frm, net, ln):
 
             color = [int(c) for c in COLORS[classIds[i]]]
             cv2.rectangle(frm, (x, y), (x + w, y + h), color, 2)
-            text = "{}: {:.4f}".format(class_names[classIds[i]], confidences[i])
-            cv2.putText(frm, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
+            text = "{}: {:.4f}".format(
+                class_names[classIds[i]], confidences[i])
+            cv2.putText(frm, text, (x, y - 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
             fps_label = "FPS: %.2f" % (1 / (end_time - start_time))
-            cv2.putText(frm, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            cv2.putText(frm, fps_label, (0, 25),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+
 
 while cv2.waitKey(1) < 1:
     (grabbed, frame) = vc.read()
     if not grabbed:
         break
-    #frame = imutils.resize(frame, 700)
+    frame = cv2.resize(frame, (args["height"], args["width"]))
     detect(frame, net, layer)
-    
+
     if args["display"] == 1:
         cv2.imshow("detections", frame)
 
     if args["output"] != "" and writer is None:
         fourcc = cv2.VideoWriter_fourcc(*"MJPG")
         writer = cv2.VideoWriter(args["output"], fourcc, 25,
-            (frame.shape[1], frame.shape[0]), True)
+                                 (frame.shape[1], frame.shape[0]), True)
 
     if writer is not None:
         writer.write(frame)
