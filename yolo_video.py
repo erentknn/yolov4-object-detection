f35313e20f7143bbf2e93dde1aae7f89c2b4e1fb
Eren <48161058+erentknn@users.noreply.github.com>
2021-05-12 14:44:50 +0300

    Update yolo_video.py

--- yolo_video.py
+++ yolo_video.py
@@ -27,9 +27,6 @@ logger.setLevel(logging.INFO)
 formatter = logging.Formatter(
     '%(asctime)s-%(name)s-%(message)s')
 
-file_handler = logging.FileHandler("deneme.log")
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
